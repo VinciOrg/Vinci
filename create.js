70f9d4cd276329a1382ac3fa0e41a845e3ba3a1e
VinciOrg <spotdayofc@gmail.com>
2026-08-16 02:46:58 -0300

    Create create.js

---
+++ create.js
@@ -0,0 +1,131 @@
+// =====================================
+// VINCI 0.5 — CREATE MENU
+// =====================================
+
+
+const openCreateMenu =
+    document.getElementById(
+        "openCreateMenu"
+    );
+
+
+const createMenu =
+    document.getElementById(
+        "createMenu"
+    );
+
+
+const closeCreateMenu =
+    document.getElementById(
+        "closeCreateMenu"
+    );
+
+
+const cancelCreate =
+    document.getElementById(
+        "cancelCreate"
+    );
+
+
+const createPublication =
+    document.getElementById(
+        "createPublication"
+    );
+
+
+const createProfilePost =
+    document.getElementById(
+        "createProfilePost"
+    );
+
+
+// =====================================
+// ABRIR
+// =====================================
+
+openCreateMenu.addEventListener(
+    "click",
+    function () {
+
+        createMenu
+            .classList
+            .remove("hidden");
+
+    }
+);
+
+
+// =====================================
+// FECHAR
+// =====================================
+
+function closeMenu() {
+
+    createMenu
+        .classList
+        .add("hidden");
+
+}
+
+
+closeCreateMenu.addEventListener(
+    "click",
+    closeMenu
+);
+
+
+cancelCreate.addEventListener(
+    "click",
+    closeMenu
+);
+
+
+// =====================================
+// CLICAR FORA
+// =====================================
+
+createMenu.addEventListener(
+    "click",
+    function (event) {
+
+        if (
+            event.target ===
+            createMenu
+        ) {
+
+            closeMenu();
+
+        }
+
+    }
+);
+
+
+// =====================================
+// PUBLICAÇÃO
+// =====================================
+
+createPublication.addEventListener(
+    "click",
+    function () {
+
+        window.location.href =
+            "post.html";
+
+    }
+);
+
+
+// =====================================
+// POST DE PERFIL
+// =====================================
+
+createProfilePost.addEventListener(
+    "click",
+    function () {
+
+        window.location.href =
+            "profile-post.html";
+
+    }
+);
